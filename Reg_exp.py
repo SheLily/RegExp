17c47a5f2c1525f977e5f3d06f95774173e19b63
SheLily <63263537+SheLily@users.noreply.github.com>
2020-07-15 10:01:45 +0300

    Update Reg_exp.py

--- Reg_exp.py
+++ Reg_exp.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 from pprint import pprint
 import re
-# читаем адресную книгу в формате CSV в список contacts_list
 import csv
+
 
 def find_pattern(s, re):
     res = re.findall(s)
@@ -11,7 +10,9 @@ def find_pattern(s, re):
         return s, res[0]
     return s, None
 
+
 def find_phone(s, ph_re, ad_re):
+
     ad = ad_re.findall(s)
     if len(ad) > 0:
         s = s.replace(ad[0], '')
@@ -20,7 +21,7 @@ def find_phone(s, ph_re, ad_re):
         phone = phone[0][1:]
         for i in phone:
             s = s.replace(i, '')
-        
+
         if len(ad) > 0:
             s = s.replace(ad[0], '')
             return s, f'{phone[0]}({phone[1]})-{phone[2]}-{phone[3]}-{phone[4]} {ad[0]}'
@@ -36,21 +37,16 @@ def get_name(s):
     return s, name
 
 
-
 with open("phonebook_raw.csv") as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 pprint(contacts_list)
-
-# TODO 1: выполните пункты 1-3 ДЗ
-# ваш код
 
 name_re = re.compile('([а-яА-Я]+)[\,\s]+([а-яА-Я]+)')
 re_s = {
         'surname': re.compile('([А-Я][а-я]+(?:ович|евич|ич|овна|евна|ична|инична))'),
-        'email' : re.compile('([\d\w\.\-\_]+@[\d\w\.\-\_]+)'),
+        'email': re.compile('([\d\w\.\-\_]+@[\d\w\.\-\_]+)'),
         'organization': re.compile('\,([а-яА-Я]+)\,'),
-        
     }
 
 phone_re = re.compile('(((?:\+7|8)).*(\d{3}).*(\d{3}).*(\d{2}).*(\d{2}))')
@@ -64,10 +60,9 @@ header = templist.pop(0)
 phonebook = {}
 for i in range(len(templist)):
     templist[i], name = get_name(templist[i])
-    
+
     if name not in phonebook:
         phonebook[name] = {}
-        
     for j in re_s:
         templist[i], info = find_pattern(templist[i], re_s[j])
         if info:
@@ -82,17 +77,10 @@ for i in range(len(templist)):
     else:
         if 'phone' not in phonebook[name]:
             phonebook[name]['phone'] = ''
-            
-    
-            
-    
+
 for i in templist:
     print(pos_re.findall(i))
-    
 
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
 with open("phonebook.csv", "w") as f:
-  datawriter = csv.writer(f, delimiter=',')
-  # Вместо contacts_list подставьте свой список
-  datawriter.writerows(contacts_list)
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(contacts_list)
